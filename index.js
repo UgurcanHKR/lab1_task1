130adb83dfab49828bdd9a9baf61b6b057a363ab
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2021-10-20 12:31:29 +0300

    run first page

--- index.js
+++ index.js
@@ -5,15 +5,10 @@ import Express from 'express';
 const app = Express();
 const port = 3000;
 app.get('/', (req,res)=> {
-   console.log('Hello World!');
+   res.send('index.html');
 })
 app.listen(port,() => {
    console.log('App listening on port 3000...');
 })
 
 
-
-ReactDOM.render(
-  <h1>Hello, world! First page</h1>,
-  document.getElementById('root')
-);
