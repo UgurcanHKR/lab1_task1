4009878cbb8ae5b3145794052ba93a3c543b0e61
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2021-10-20 21:24:55 +0300

    detailed comments are added to each row

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
+// Each module is installed via "npm install (module name)"
 const express = require("express");
 const {MongoClient} = require('mongodb');
 const app = express();
 const port = 3000;
-
+// mongodb url has username:password and database name
 var url = "mongodb+srv://dbUser:GIfZyEKRLZlwUueW@"+
 "cluster0.tt2bl.mongodb.net/Cargo?"+
 "retryWrites=true&w=majority";
-
+// this function calls the database from mongodb and
+// calls another function for retrieving data from "cargo_info" table
 var invokeDatabase = function(callback) {
-    
+    // with Mongodb module and connect function, we get the connection with url info
     MongoClient.connect(url, function(err, db) {
     db = db.db("Cargo");
     if(err) {
@@ -16,37 +18,41 @@ var invokeDatabase = function(callback) {
         callback(err, null);
         return;
     }    
+    // Information for successful connection 
     console.log("Connected successfully to database server");
-
+    // This function calls select specified collection and its all elements
     selectDataFiltered(db, function(err, data) {
         callback(err, data);
     });
     db.close;
     });
 };
-
+// "Cargo_info" database collection is selected by find function
 var selectDataFiltered = function(db, callback){
-    
+    // the given collection is converted into a normal array
     db.collection("cargo_info").find({}).toArray (function(err, result) {
         if (err) throw err;
         callback(err, result);        
     });
 
 };
-
+// This function provides to define a route handler for GET reqs to a given URL
 app.get('', function(req, res) {
     console.log("Someone connected.")
+    // Set up database connection using this function
     invokeDatabase(function(err, data) {
         if(err)
+            // if con is unable, standard error is given
             res.status(500).json({error: err});
         else
+            // if con is ok, the data is presented with json format
             res.json(data);
     }
     );
         //res.send('Welcome G')
 });
 
-
+// With this function provides that this application is running on specified port
 app.listen(port, () => {
    console.log('App listening on port 3000...');
 })
