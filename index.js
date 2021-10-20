c103c971ac4e44bb7fbdcf3caebaaf9f1204849f
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2021-10-20 11:58:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// const express = require('express');
+// const app = express();
+// app.get('/', (req,res)=> {
+//   console.log('Hello World!');
+// })
+// app.listen(3000,() => {
+//   console.log('App listening on port 3000...Nodemon');
+// })
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
