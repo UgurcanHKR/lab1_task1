1f8e5e04b9a7878ea3c18132169b00dce41780c4
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2021-10-20 21:08:50 +0300

    db con and fetchData from mongodb is done

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
 const express = require("express");
-const path = require('path');
+const {MongoClient} = require('mongodb');
 const app = express();
-const port = 3000;
+const port = 8000;
 
-app.get('/', function(req, res){
-    var options = {
-        root: path.join(__dirname)
-    };
-     
-    var fileName = 'index.html';
-    res.sendFile(fileName, options, function (err) {
-        if (err) {
-            next(err);
-        } else {
-            console.log('Sent:', fileName);
-        }
+var url = "mongodb+srv://dbUser:GIfZyEKRLZlwUueW@"+
+"cluster0.tt2bl.mongodb.net/Cargo?"+
+"retryWrites=true&w=majority";
+
+var invokeDatabase = function(callback) {
+    
+    MongoClient.connect(url, function(err, db) {
+    db = db.db("Cargo");
+    if(err) {
+        console.log("Unable to connect database");
+        callback(err, null);
+        return;
+    }    
+    console.log("Connected successfully to database server");
+
+    selectDataFiltered(db, function(err, data) {
+        callback(err, data);
+        
     });
+    db.close;
+    });
+};
+
+var selectDataFiltered = function(db, callback){
+    
+    db.collection("cargo_info").find({}).toArray (function(err, result) {
+        if (err) throw err;
+        callback(err, result);        
+    });
+
+};
+
+app.get('', function(req, res) {
+    console.log("Someone connected.")
+    invokeDatabase(function(err, data) {
+        if(err)
+            res.status(500).json({error: err});
+        else
+            res.json(data);
+    }
+    );
+        //res.send('Welcome G')
 });
 
 
 app.listen(port, () => {
-   console.log('App listening on port 3000...');
+   console.log('App listening on port 8000...');
 })
 
 
