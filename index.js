6115f0e6dbbbd70c97cba7d7c43a0d625d8a1219
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2021-10-20 12:31:52 +0300

    start first page

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import Express from 'express';
 
 const app = Express();
 const port = 3000;
-app.get('/', (req,res)=> {
-   res.send('index.html');
+
+app.get('', (req,res)=> {
+   res.sendFile('index.html');
 })
+
+
 app.listen(port,() => {
    console.log('App listening on port 3000...');
 })
