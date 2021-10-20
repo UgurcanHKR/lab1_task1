b71ed6c268ae2e77326972a5960208707a14dbaf
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2021-10-20 12:56:17 +0300

    finish lab1_task1

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-import Express from 'express';
-
-const app = Express();
+const express = require("express");
+const path = require('path');
+const app = express();
 const port = 3000;
 
-app.get('', (req,res)=> {
-   res.sendFile('index.html');
-})
+app.get('/', function(req, res){
+    var options = {
+        root: path.join(__dirname)
+    };
+     
+    var fileName = 'index.html';
+    res.sendFile(fileName, options, function (err) {
+        if (err) {
+            next(err);
+        } else {
+            console.log('Sent:', fileName);
+        }
+    });
+});
 
 
-app.listen(port,() => {
+app.listen(port, () => {
    console.log('App listening on port 3000...');
 })
 
