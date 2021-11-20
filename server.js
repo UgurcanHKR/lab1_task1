9c53d83ecb73c3750e37edbc8c6e4b55d113d177
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2021-11-20 18:04:35 +0300

    updates

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(measurementsRouter);
 
 connectToDatabase()
     .then(() => {
-        const PORT = process.env.PORT || 8080;
+        const PORT = process.env.PORT || 8083;
         app.listen(PORT, () => {
             console.log('Listening to port ' + PORT);
         });
