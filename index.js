4ab89da25b8bc440c12bb005b0161076c7601f9e
UgurcanHKR <ugurcan.sencan0001@stud.hkr.se>
2021-11-20 14:12:03 +0300

    updates

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Each module is installed via "npm install (module name)"
+// Each module is installed by "npm install (module name)"
 const express = require("express");
 const path = require('path');
 const app = express();
@@ -7,14 +7,16 @@ const port = 3000;
 // This function provides to define a route handler for GET reqs to a given URL
 app.get('/', function(req, res){
 
-    // 
+    // A variable for the absolute path of the directory, 
+    // containing the currently executing file.
     var options = {
         root: path.join(__dirname)
     };
-    // a variable for specified html page
+    console.log(__dirname);
+    // A variable for specified html page
     var fileName = 'index.html';
 
-    // With this stetement, we can transfer a file at the given path
+    // With this statement, we can transfer a file at the given path
     res.sendFile(fileName, options, function (err) {
         if (err) {
             next(err);
@@ -24,7 +26,7 @@ app.get('/', function(req, res){
     });
 });
 
-// With this function provides that this application is running on specified port
+// This function provides that this application is running on specified port
 app.listen(port, () => {
 
    // Information for listening the application 
